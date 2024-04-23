66969f7ecbb11d2173a88c7445ee2f80886bc4d9
Her Fri <herbert.frichert@outlook.de>
2024-04-23 17:53:09 +0200

    build form and handling submissions

--- src/App.js
+++ src/App.js
@@ -20,9 +20,27 @@ function Logo() {
 }
 
 function Form() {
-  return <div className="add-form">
-    <h3>What do you need for your 😍 trip?</h3>
-  </div>
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(e);
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your 😍 trip?</h3>
+      <select>
+        {Array.from({length: 20}, (_, i) => i + 1).
+        map(num=> (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
+    </form>
+  );
 }
 
 function PackingList() {
@@ -30,7 +48,7 @@ function PackingList() {
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <Item item={item} />
+          <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>
