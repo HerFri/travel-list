179e8139f5f8a58fbd5eeb581cb82092857c233e
Her Fri <herbert.frichert@outlook.de>
2024-04-23 18:52:24 +0200

    add controlled elements

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: true },
   { id: 2, description: "Socks", quantity: 12, packed: true },
@@ -20,16 +22,27 @@ function Logo() {
 }
 
 function Form() {
-
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(e);
+
+    if(!description) return;
+
+    const newItem ={description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    setDescription("");
+    setQuantity(1);
   }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-      <select>
+      <select
+      value={quantity}
+      onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({length: 20}, (_, i) => i + 1).
         map(num=> (
           <option value={num} key={num}>
@@ -37,7 +50,11 @@ function Form() {
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input type="text" placeholder="Item..."
+      value={description} 
+      onChange={(e) => 
+        setDescription(e.target.value)}
+      />
       <button>Add</button>
     </form>
   );
